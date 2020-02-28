6dbd9c9f39e807afd38c223c778e1940a3795286
vcarl <vcarlos139@gmail.com>
2020-02-28 12:08:38 +0100

    prueba

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,9 +1,11 @@
 public class Triangulo {
     private double b;
     private double h;
+    private double prueba;
 
     public Triangulo(double base, double height){
         this.b=base;
         this.h=height;
+        this.prueba=base;
     }
 }
