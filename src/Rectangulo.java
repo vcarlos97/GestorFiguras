f0cb587e6a3237219bf4bbbbcb4c46220e52f121
irega97 <ivanreq97@gmail.com>
2020-03-02 12:18:51 +0100

    Funcion area Triangulo

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,9 +1,19 @@
-public class Rectangulo {
+public class Rectangulo extends Figura{
     private double l1;
     private double l2;
 
     public Rectangulo(double l1, double l2){
         this.l1=l1;
         this.l2=l2;
+    }
+
+    @Override
+    public double area() {
+        return (l1*l2);
+    }
+
+    public static void main(String[] args){
+        Rectangulo r = new Rectangulo(4,10);
+        System.out.println(r.area());
     }
 }
