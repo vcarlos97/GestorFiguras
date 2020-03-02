a67095094721004195921ddf43743f901d57fe71
vcarl <vcarlos139@gmail.com>
2020-03-02 12:28:49 +0100

    Optimizado

--- src/Circulo.java
+++ src/Circulo.java
@@ -5,16 +5,11 @@ public class Circulo extends Figura{
         this.r = r;
     }
 
-    public double getAreaCirculo() {
-        double a = Math.PI * r * r;
-        return a;
-    }
-
-    @Override
     public double area() {
-        return getAreaCirculo();
-    }
+        return  Math.PI * r * r;
 
+    }
+    
     public static void main(String[] args) {
         Circulo c = new Circulo(4);
         System.out.println(c.area());
--- src/Triangulo.java
+++ src/Triangulo.java
@@ -7,13 +7,8 @@ public class Triangulo extends Figura {
         this.h = height;
     }
 
-    public double getAreaTriangle() {
-        return (b * h) / 2;
-    }
-
-    @Override
     public double area() {
-        return getAreaTriangle();
+        return (b * h) / 2;
     }
 
     public static void main(String[] args) {
